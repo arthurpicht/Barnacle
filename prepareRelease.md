1d7b0a1af8b48b8bbbd46a118abf5460b7ad3622
Arthur Picht <picht@arthurpicht.de>
2020-01-23 10:16:54 +0100

    changes for dev on v0.2.2-SNAPSHOT

--- prepareRelease.md
+++ prepareRelease.md
@@ -6,6 +6,8 @@
 
 * Edit *build.gradle* set Version
 
+* Edit *release-history.md*
+
 * Commit to GitHub and draft release
 
 * Edit all mentioned files again and give new Version Number with -SNAPSHOT
--- src/main/java/de/arthurpicht/barnacle/BarnacleInitializer.java
+++ src/main/java/de/arthurpicht/barnacle/BarnacleInitializer.java
@@ -35,7 +35,7 @@ public class BarnacleInitializer {
     public static enum Encoding {ISO, UTF, DEFAULT}
 
 //    public static final String VERSION = "Barnacle Version 0.2.1-snapshot (2019.11)";
-    public static final String VERSION = "Barnacle Version 0.2.1 (2020-01-23)";
+    public static final String VERSION = "Barnacle Version 0.2.2-SNAPSHOT (2020-01-23)";
 
     private static Logger logger = LoggerFactory.getLogger("BARNACLE");
 
